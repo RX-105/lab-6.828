e190cd759f56be80826e77a8244a331ecfcc5f9f
欸嘿欸嘿 <1436648676@qq.com>
2022-10-16 23:14:43 +0800

    today's test

--- TestCode/test.c
+++ TestCode/test.c
@@ -3,6 +3,7 @@ typedef uint32_t pde_t;
 typedef uint32_t pte_t;
 
 #include <stdio.h>
+#include <stdlib.h>
 
 __attribute__ ((constructor)) void
 before(void){
@@ -14,23 +15,10 @@ after(void){
     printf("run after main()\n");
 }
 
-void
+int
 main (void)
 {
-    pde_t entry_pgdir[10] = {
-        [0] = 1,
-        [9] = 114514};
-    pte_t entry_pgtable[10] = {
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-    uint32_t i = 0;
-    for (; i < 10; ++i)
-    {
-        printf("%d: %d\n", i, entry_pgdir[i]);
-    }
-    uint32_t a = 0xF0000000 >> 22;
-    printf("%d\n", a);
-    printf("%d\n", 0x002000 | 0x001 | 0x002);
-    register int b = 0xFFFFFFFF;
-    printf("%d\n", b);
-    printf("%d\n", -1*10+1);
+    char *str = (char *)malloc(sizeof(char));
+    *str = 'c';
+    printf("%s", str);
 }
